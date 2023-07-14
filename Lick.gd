5e6eb101a240f91a704d0979bd22eacb3b7714f9
Davi Fox <davifoox@gmail.com>
2023-07-13 21:04:06 -0300

    .

--- Lick.gd
+++ Lick.gd
@@ -1,27 +1,26 @@
 extends Control
 
-
 onready var button = $Button
 onready var audio_stream_player = $AudioStreamPlayer
+onready var tags_container = $TagsContainer
 
-enum tags {
-	#Chord
-	I,
-	IV,
-	V,
-	
-	#Music
-	orangeDudeBlues
-}
-
-export(tags) var lick_tags
+const TAGS_PATH = "res://tags/tags/"
+const FILE_EXTENSION = "tres"
 
 func _ready() -> void:
 	button.connect("pressed", self, "_on_button_pressed")
-	
-	var tag_name = tags.keys()[tags.I]
-	add_to_group(tag_name)
-	print(tag_name)
-	
+	get_tags_and_add_parent_to_groups()
+		
 func _on_button_pressed():
 	audio_stream_player.play()
+
+func get_tags_and_add_parent_to_groups():
+	for tag in tags_container.tags_list:
+		var tag_name = tag.resource_path
+		
+		tag_name = tag_name.lstrip(TAGS_PATH)
+		tag_name = tag_name.rstrip(FILE_EXTENSION)
+		tag_name = tag_name.rstrip(".")
+		get_parent().add_to_group(tag_name)
+		
+		print(tag_name)
---
+++ tags/Tag.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Tag
+
+func get_name():
+	return resource_name
