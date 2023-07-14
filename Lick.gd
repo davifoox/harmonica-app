534537a961691f0f39457383b525010c4f0b1fdc
Davi Fox <davifoox@gmail.com>
2023-07-13 21:09:59 -0300

    .

--- Lick.gd
+++ Lick.gd
@@ -1,6 +1,9 @@
 extends Control
 
+export(String) var lick_name
+
 onready var button = $Button
+onready var label = $Label
 onready var audio_stream_player = $AudioStreamPlayer
 onready var tags_container = $TagsContainer
 
@@ -9,18 +12,19 @@ const FILE_EXTENSION = "tres"
 
 func _ready() -> void:
 	button.connect("pressed", self, "_on_button_pressed")
-	get_tags_and_add_parent_to_groups()
+	get_tags_and_add_as_groups()
+	label.text = lick_name
 		
 func _on_button_pressed():
 	audio_stream_player.play()
 
-func get_tags_and_add_parent_to_groups():
+func get_tags_and_add_as_groups():
 	for tag in tags_container.tags_list:
 		var tag_name = tag.resource_path
 		
 		tag_name = tag_name.lstrip(TAGS_PATH)
 		tag_name = tag_name.rstrip(FILE_EXTENSION)
 		tag_name = tag_name.rstrip(".")
-		get_parent().add_to_group(tag_name)
 		
+		add_to_group(tag_name)
 		print(tag_name)
