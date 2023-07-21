287fafc1a93ff671150d89109134b953506badd7
Davi Fox <davifoox@gmail.com>
2023-07-21 18:44:31 -0300

    add - list with pitches for every note

--- KeyController.gd
+++ KeyController.gd
@@ -1,11 +1,9 @@
-extends Node
+extends Control
 
 var note_step = 0.06
 export var current_note = 1
 onready var audio = $AudioStreamPlayer
-
-var key_names_list = ["LF", "Gb",  "G", "Ab", "A", "Bb",  "B",  "C", "Db",  "D", "Eb",  "E",  "F"]
-var pitches_list =   [0.79, 0.84, 0.89, 0.94,   1, 1.06, 1.12, 1.18, 1.26, 1.33, 1.41, 1.49, 1.58]
+signal key_changed(key_name, pitch)
 
 func _ready() -> void:
 	audio.pitch_scale = current_note
@@ -22,3 +20,4 @@ func _process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed("ui_accept"):
 		audio.play()
+
