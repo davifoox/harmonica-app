5ae1923fbcbcdb84d3ba225c1b4c71808817df8c
Davi Fox <davifoox@gmail.com>
2023-07-21 18:58:20 -0300

    add - all licks key changing

--- KeyController.gd
+++ KeyController.gd
@@ -1,23 +1,27 @@
 extends Control
 
-var note_step = 0.06
-export var current_note = 1
+export var current_key = 4
 onready var audio = $AudioStreamPlayer
+onready var key_label = $KeyLabel
+
 signal key_changed(key_name, pitch)
+var key_names_list = ["LF", "Gb",  "G", "Ab", "A", "Bb",  "B",  "C", "Db",  "D", "Eb",  "E",  "F"]
+var pitches_list =   [0.79, 0.84, 0.89, 0.94,   1, 1.06, 1.12, 1.18, 1.26, 1.33, 1.41, 1.49, 1.58]
 
 func _ready() -> void:
-	audio.pitch_scale = current_note
+	audio.pitch_scale = pitches_list[current_key]
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_up"):
-		current_note += note_step
-		audio.pitch_scale = current_note
-		audio.play()
-	elif Input.is_action_just_pressed("ui_down"):
-		current_note -= note_step
-		audio.pitch_scale = current_note		
-		audio.play()
-		
-	if Input.is_action_just_pressed("ui_accept"):
-		audio.play()
+func _on_ButtonPlus_pressed() -> void:
+	change_note(1)
 
+func _on_ButtonMinus_pressed() -> void:
+	change_note(-1)
+
+func change_note(step : int):
+	current_key += step
+	current_key = clamp(current_key, 0, key_names_list.size()-1)
+	emit_signal("key_changed", key_names_list[current_key], pitches_list[current_key])
+	
+	key_label.text = key_names_list[current_key]
+	audio.pitch_scale = pitches_list[current_key]
+	audio.play()
--- Main.gd
+++ Main.gd
@@ -4,6 +4,7 @@ var audio_files_list = []
 var lick_scene = preload("res://licks/Lick.tscn")
 onready var licks_container = $ScrollContainerSwipe/LicksContainer
 const AUDIO_FOLDER_PATH = "res://licks/audio/"
+onready var key_controller = $KeyController
 
 func _ready() -> void:
 	audio_files_list = Util.load_files_paths(AUDIO_FOLDER_PATH)
@@ -13,6 +14,8 @@ func _ready() -> void:
 		l.init(audio_file)
 		licks_container.add_child(l)
 		l.connect("started_playing", self, "_on_lick_started_playing")
+	
+	key_controller.connect("key_changed", self, "_on_KeyController_key_changed")
 
 func _on_lick_started_playing(node_name):
 	for lick in licks_container.get_children():
@@ -20,3 +23,6 @@ func _on_lick_started_playing(node_name):
 			if lick.audio_stream_player.playing:
 				lick.audio_stream_player.stop()
 				
+func _on_KeyController_key_changed(key_name: String, pitch : float):
+	for lick in licks_container.get_children():
+		lick.audio_stream_player.pitch_scale = pitch
